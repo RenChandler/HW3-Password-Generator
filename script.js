a32505cf999f5d0c776b69d625187f38dbc4a2d1
Ren <rnchandramouli42@gmail.com>
2021-04-30 17:44:54 -0600

    adding global variables and started prompts for password length

--- script.js
+++ script.js
@@ -1,11 +1,104 @@
-// Assignment Code
+//Global Variables
+var lowercase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+var Uppercase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+var numerics = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialchar = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+function passwordPrompt() {
+  //Variable to store the link of the password
+
+  var length = parseInt(prompt("How many characters would you like the password to be?"));
+
+   if (length <8 || length >128){
+     alert("Password has to be between 8 and 128 characters");
+     return;
+   }
+
+
+}
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = ;
 
   passwordText.value = password;
 
